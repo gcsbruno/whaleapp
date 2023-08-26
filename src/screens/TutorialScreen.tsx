f9e9447efdc1de94a576e806e3b3d8cc37665044
Bruno <brunoguimaraes.cs@gmail.com>
2023-08-25 23:25:12 -0300

    UI tutorial screen with slider

--- src/screens/TutorialScreen.tsx
+++ src/screens/TutorialScreen.tsx
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, ImageSourcePropType } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image, } from 'react-native'
 import Swiper from 'react-native-swiper'
-import LoginScreen from './LoginScreen';
+import Button from '../components/buttons/Button';
 
 
 
@@ -13,74 +13,98 @@ const TutorialScreen = () => {
         console.log('next page')
     }
 
-
-    const newLocal = <Text style={styles.tutorialText}>Passo 1: Descrição da etapa 1</Text>;
     return (
         <View style={styles.container}>
-            <Swiper style={styles.swiperContainer}>
-                {/* Etapa 1 */}
-                <View style={styles.slide}>
-                  
+            <View style={styles.header}>
+                <Image source={require('../../assets/icons/whale.png')} style={styles.logo} />
+                <Image source={require('../../public/images/whale.png')} style={styles.logo} />
+            </View>
+            <Swiper>
+                <View style={styles.slide1}>
+                    <View style={styles.content}>
+                        <Image style={styles.image} source={require('../../public/images/carrousel1.png')} />
+                        <Text style={styles.title}> O aplicativo perfeito para planejar sua viagem completa em um único lugar!</Text>
+                    </View>
                 </View>
-
-                {/* Etapa 2 */}
-                <View style={styles.slide}>
-
-                    <Text style={styles.tutorialText}>Passo 2: Descrição da etapa 2</Text>
+                <View style={styles.slide2}>
+                    <View style={styles.content}>
+                        <Image style={styles.image} source={require('../../public/images/carrousel2.png')} />
+                        <Text style={styles.title}> Planeje sua viajem com seus amigos/familiares e separe os gastos </Text>
+                    </View>
                 </View>
-
-                {/* Etapa 3 */}
-                <View style={styles.slide}>
-                    
-                    <Text style={styles.tutorialText}>Passo 3: Descrição da etapa 3</Text>
+                <View style={styles.slide3}>
+                    <View style={styles.content}>
+                        <Image style={styles.image} source={require('../../public/images/carrousel3.png')} />
+                        <Text style={styles.title}> Tenha as melhores ofertas de hotéis, restaurantes, passeios e muito mais!</Text>
+                    </View>
                 </View>
-
-                {/* Etapa 4 */}
-                <View style={styles.slide}>
-                    
-                    <Text style={styles.tutorialText}>Passo 4: Descrição da etapa 4</Text>
+                <View style={styles.slide4}>
+                    <View style={styles.content}>
+                        <Image style={styles.image} source={require('../../public/images/carrousel4.png')} />
+                        <Text style={styles.title}> Defina seus gastos e conte com nossas sugestões baseadas no seu perfil.</Text>
+                    </View>
                 </View>
             </Swiper>
+            <Button title='Finalizar' type='cta' onPress={() => { }} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    wrapper: {},
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#227BE5'
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#227BE5'
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#227BE5'
+    },
+    slide4: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#227BE5'
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#227BE5',
     },
-    swiperContainer: {
-        flex: 1,
-        width: Dimensions.get('window').width, // Faz o carrossel ocupar a largura da tela
-    },
-    slide: {
-        flex: 1,
-        justifyContent: 'center',
+    header: {
+        flexDirection: 'row',
         alignItems: 'center',
+        padding: 4,
+        marginTop: 32,
+    },
+    logo: {
+        marginHorizontal: 4,
+    },
+    title: {
+        color: '#fff',
+        marginLeft: 8,
+        fontSize: 18,
+    },
+    content: {
+        alignItems: 'center',
+        marginHorizontal: 24,
+        marginVertical: 24,
+        padding: 24
     },
     image: {
-        width: '80%',
-        height: '50%',
-        resizeMode: 'contain',
-    },
-    tutorialText: {
-        marginTop: 20,
-        fontSize: 18,
-        textAlign: 'center',
-    },
-    button: {
-        marginTop: 20,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        backgroundColor: 'blue',
-        borderRadius: 10,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-    },
+        marginVertical: 32
+    }
 });
 
 export default TutorialScreen;
